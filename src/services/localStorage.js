c7fa37193ff4a3000cd1bb3aad493296f2abe954
gmarcos87 <gmarcos87@gmail.com>
2019-07-30 00:50:22 -0300

    add clear and delete localStorage

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -25,8 +25,26 @@ export function setStorage (area, data) {
         try {
             const rawData = JSON.stringify(data)
             localStorage.setItem(area, rawData)
+            res({status: 'ok'})
         } catch (error) {
             rej(error);
         }
+    })
+}
+
+/**
+ *  @param {String} area
+ */
+export function deleteStorage (area) {
+    return new Promise((res, rej)=>{
+            localStorage.removeItem(area)
+            res({status: 'ok'})
+    })
+}
+
+export function clearStorage () {
+    return new Promise((res, rej)=>{
+            localStorage.clear()
+            res({status: 'ok'})
     })
 }
